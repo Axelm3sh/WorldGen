48507d0ddab9d0ed7ca6c23c7404e05ad2e2f6bf
Axelm3sh <axel.m3sh@gmail.com>
2017-06-14 12:05:22 -0700

    Start project

---
+++ cmake-build-debug/CMakeFiles/WorldGen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WorldGen.dir/main.cpp.obj"
+  "WorldGen.pdb"
+  "WorldGen.exe"
+  "WorldGen.exe.manifest"
+  "libWorldGen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WorldGen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/WorldGen.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WorldGen.
+# This may be replaced when dependencies are built.
